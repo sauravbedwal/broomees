01e5ba5c0075ce8293aa310381b788fc7c2c2a47
Saurav Bedwal <120270290+sauravbedwal@users.noreply.github.com>
2024-11-18 05:16:38 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,13 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-app.use(cors(
-  {
-    origin: ["https://broomees-ivory.vercel.app/"],
-    methods: ["POST", "GET"],
-    credentials: true
-  }
-));
+app.use(cors());
 
 const connectDB = require("./config/database");
 
